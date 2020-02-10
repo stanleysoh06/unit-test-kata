5a45fa2181cc477dc3768ed35241edbf6dad2e5f
Stanley Soh <stanleysoh@Stanleys-MacBook-Air.local>
2020-02-10 15:01:43 +0800

    refractor code

--- ruby/lib/word_count.rb
+++ ruby/lib/word_count.rb
@@ -4,8 +4,7 @@ class WordCount
 
     if input != ''
       words = input.split(" ")
-      word_count = words.each_with_object(Hash.new(0)) {|word, count| count[word] += 1}
-      word_count
+      words.each_with_object(Hash.new(0)) {|word, count| count[word] += 1}
     else
       'n/a'
     end
